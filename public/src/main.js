486615182c8f36dc82291eadb21a92e9323fc54d
kingofbanania <wegosci@gmail.com>
2018-05-12 13:40:40 -0400

    Added basic navigation functionality

--- public/src/main.js
+++ public/src/main.js
@@ -5,3 +5,48 @@ if('serviceWorker' in navigator) {
     });
 }
 
+window.addEventListener('load',()=>{
+    $('#home').show();
+    $('#camera').hide();
+    $('#search').hide();
+    $('#community').hide();
+    $('#profile').hide();
+});
+
+function navigate(el) {
+    if (el.id=="homeBtn") {
+        $('#home').show();
+        $('#camera').hide();
+        $('#search').hide();
+        $('#community').hide();
+        $('#profile').hide();
+    }
+    else if (el.id=="cameraBtn") {
+        $('#home').hide();
+        $('#camera').show();
+        $('#search').hide();
+        $('#community').hide();
+        $('#profile').hide();
+    }
+    else if (el.id=="searchBtn") {
+        $('#home').hide();
+        $('#camera').hide();
+        $('#search').show();
+        $('#community').hide();
+        $('#profile').hide();
+    }
+    else if (el.id=="communityBtn"){
+        $('#home').hide();
+        $('#camera').hide();
+        $('#search').hide();
+        $('#community').show();
+        $('#profile').hide();
+    }
+    else if (el.id=="profileBtn"){
+        $('#home').hide();
+        $('#camera').hide();
+        $('#search').hide();
+        $('#community').hide();
+        $('#profile').show();
+    }
+}
